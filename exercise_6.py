20395753e722361960821124dfcb64527adc7dd5
doydoydoy <colourd.eyes@gmail.com>
2019-09-21 14:01:02 +0800

    added exercise files

---
+++ exercise_6.py
@@ -0,0 +1,22 @@
+# Challenge - Dictionaries Exercise
+#
+# Given two lists - words and definitions,
+# Create a dictionary called cooldictionary where you
+# use words for keys and definitions for values
+
+
+words = ["PoGo","Spange","Lie-Fi"]
+definitions = ["Slang for Pokemon Go","To collect spare change, either from couches, passerbys on the street or any numerous other ways and means","When your phone or tablet indicates that you are connected to a wireless network, however you are still unable to load webpages or use any internet services with your device"]
+
+def create_dictionary(words, definitions):
+	pairs = {}
+
+	for n in range(len(words)):
+		pairs[words[n]] = definitions[n]
+
+	return pairs
+
+
+cooldictionary = create_dictionary(words, definitions)
+
+print(cooldictionary)
